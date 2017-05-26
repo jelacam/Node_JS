d18b53d3cc743a200bd0bf8dea4e30a52be37180
Marko Jelača <jelaca.marko@gmail.com>
2017-05-26 15:19:59 +0200

    Upotreba JS class umesto React.createClass

--- Node&React/views/index.jsx
+++ Node&React/views/index.jsx
@@ -1,14 +1,16 @@
 var React = require('react');
 
-var IndexComponent = React.createClass({
-    render: function(){
+// React 16 izbacuje createClass - koriste se JavaScript klase 
+class IndexComponent extends React.Component{
+    render() {
         return (
             <div>
                 <h1> this was built using react </h1>
-                <h2> By Marko Jelaca</h2>
+                <h2> By Marko Jelaca </h2>
+                <h2> 26.6.2017 </h2>
             </div>
         )
     }
-});
+};
 
 module.exports = IndexComponent;
